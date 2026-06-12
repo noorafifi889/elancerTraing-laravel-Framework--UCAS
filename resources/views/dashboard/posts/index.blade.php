7ce7e56115a8625eb671a03c550c19e0747dc944
noorafifi889 <noorelafifi889@gmail.com>
2026-06-12 17:19:51 +0300

    paginatition

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -238,7 +238,7 @@
                             </div>
                         </div>
                     @endforeach
-{{ $post->links() }}
+{{ $posts->links() }}
 
                     </div >
                 <div class="flex items-center justify-between pt-8">
