7a666ec1b91deb4691d33c54c74afdcbe2ae8542
noorafifi889 <noorelafifi889@gmail.com>
2026-06-23 22:19:14 +0300

    add a following and follower funtion to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,13 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -31,10 +34,36 @@ class User extends Authenticatable
         ];
     }
 
+
+public function followings()
+{
+    return $this->belongsToMany(
+        User::class,
+        'followers',
+        'follower_id',
+        'following_id'
+    );
+}
+
+public function followers()
+{
+    return $this->belongsToMany(
+        User::class,
+        'followers',
+        'following_id', // الشخص الذي يتم متابعته
+        'follower_id'   // الشخص الذي يتابع
+    );
+}
+
     public function posts(): HasMany
     {
-        return $this->hasMany(Post::class ,"user_id",'id');
+        return $this->hasMany(Post::class, "user_id", 'id');
     }
-
-
+public function avatarUrl(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->avatar ? Storage::disk('public')->url($this->avatar) : asset('images/avatars/blank.png')
+        );
+    }
+    
 }
